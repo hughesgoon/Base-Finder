1ebabf6e640a7bd51438b81c7990466ce1f0d7ef
Hyeonjae.Kim <hyeonjae.dev@gmail.com>
2021-06-06 08:02:43 +0000

    [feat] Setup DB connection
    Install mysql, bcryptjs
    Add mysql connection object

--- backend/app.js
+++ backend/app.js
@@ -9,6 +9,25 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+var mysql = require('mysql');
+require('dotenv').config();
+
+var connection = mysql.createConnection({
+	host : 'localhost',
+	port : 3306,
+	user : process.env.DATABASE_USERNAME,
+	password : process.env.DATABASE_PASSWORD,
+	database : process.env.DATABASE_NAME
+});
+
+connection.connect(function (err){
+	if (err) {
+		console.error('mysql connection error');
+		console.error(err);
+		throw err;
+	}
+});
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
